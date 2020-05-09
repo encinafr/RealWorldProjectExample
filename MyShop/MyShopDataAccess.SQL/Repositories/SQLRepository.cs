acec62fa63f1c61fe6634778d7d24aefb242134a
Javier Encina <encinajavier7@gmail.com>
2020-05-09 19:56:10 -0300

    Add SQLRepository

---
+++ MyShop/MyShopDataAccess.SQL/Repositories/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopDataAccess.SQL.Repositories
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext _context;
+        internal DbSet<T> _dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+           _context = context;
+           _dbSet = _context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return _dbSet;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (_context.Entry(t).State == EntityState.Detached)
+                _dbSet.Attach(t);
+
+            _dbSet.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return _dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            _dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            _dbSet.Attach(t);
+            _context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
