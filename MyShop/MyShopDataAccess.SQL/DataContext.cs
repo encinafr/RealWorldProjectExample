3ec82ad207b54742dfec501d758ae2e131fde654
Javier Encina <encinajavier7@gmail.com>
2020-05-09 18:02:17 -0300

    Add EF

---
+++ MyShop/MyShopDataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopDataAccess.SQL
+{
+    public class DataContext : DbContext
+    { 
+        public DataContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DataContext>());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
