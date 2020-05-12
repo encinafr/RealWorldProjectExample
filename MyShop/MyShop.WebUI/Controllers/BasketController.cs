e96f7667e030be42ed84a6e8fa61c557ef780358
Javier Encina <encinajavier7@gmail.com>
2020-05-11 23:05:04 -0300

    Cerate Basket UI

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketSerivce _basketSerivce;
+
+        public BasketController(IBasketSerivce basketSerivce)
+        {
+            _basketSerivce = basketSerivce;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = _basketSerivce.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            _basketSerivce.AddToBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            _basketSerivce.RemoveBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = _basketSerivce.GetBasketSummary(this.HttpContext);
+            return PartialView(basketSummary);
+        }
+    }
+}
